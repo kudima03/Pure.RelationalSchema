f52e61848ef5d3aa277c37fd9448c44f352f83b9
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-24 12:03:00 +0300

    Added tests for UShortColumnType.

---
+++ src/Tests/Pure.RelationalSchema.Tests/ColumnType/UShortColumnTypeTests.cs
@@ -0,0 +1,28 @@
+﻿using Pure.Primitives.Materialized.String;
+using Pure.RelationalSchema.Abstractions.ColumnType;
+using Pure.RelationalSchema.ColumnType;
+
+namespace Pure.RelationalSchema.Tests.ColumnType;
+
+public sealed record UShortColumnTypeTests
+{
+    [Fact]
+    public void ContainCorrectTypeName()
+    {
+        const string expectedTypeName = "ushort";
+        IColumnType columnType = new UShortColumnType();
+        Assert.Equal(expectedTypeName, new MaterializedString(columnType.Name).Value);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new UShortColumnType().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new UShortColumnType().ToString());
+    }
+}
