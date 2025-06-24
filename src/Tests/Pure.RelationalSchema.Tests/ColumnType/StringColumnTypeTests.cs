87198b7da1d5c5d287f14631b3937653241b1aed
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-24 12:02:08 +0300

    Added tests for StringColumnType.

---
+++ src/Tests/Pure.RelationalSchema.Tests/ColumnType/StringColumnTypeTests.cs
@@ -0,0 +1,28 @@
+﻿using Pure.Primitives.Materialized.String;
+using Pure.RelationalSchema.Abstractions.ColumnType;
+using Pure.RelationalSchema.ColumnType;
+
+namespace Pure.RelationalSchema.Tests.ColumnType;
+
+public sealed record StringColumnTypeTests
+{
+    [Fact]
+    public void ContainCorrectTypeName()
+    {
+        const string expectedTypeName = "string";
+        IColumnType columnType = new StringColumnType();
+        Assert.Equal(expectedTypeName, new MaterializedString(columnType.Name).Value);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new StringColumnType().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new StringColumnType().ToString());
+    }
+}
