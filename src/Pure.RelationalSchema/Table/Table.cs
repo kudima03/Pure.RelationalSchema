0f5c1ae908a2f2d60f7484e290047f1377a6ef01
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-24 12:21:30 +0300

    Implemented Table.

---
+++ src/Pure.RelationalSchema/Table/Table.cs
@@ -0,0 +1,32 @@
+﻿using Pure.Primitives.Abstractions.String;
+using Pure.RelationalSchema.Abstractions.Column;
+using Pure.RelationalSchema.Abstractions.Index;
+using Pure.RelationalSchema.Abstractions.Table;
+
+namespace Pure.RelationalSchema.Table;
+
+public sealed record Table : ITable
+{
+    public Table(IString name, IEnumerable<IColumn> columns, IEnumerable<IIndex> indexes)
+    {
+        Name = name;
+        Columns = columns;
+        Indexes = indexes;
+    }
+
+    public IString Name { get; }
+
+    public IEnumerable<IColumn> Columns { get; }
+
+    public IEnumerable<IIndex> Indexes { get; }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
