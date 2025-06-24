276034a5a2a9a22294aba2b532635e75fe170a6a
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-24 12:02:31 +0300

    Added tests for TimeColumnType.

---
+++ src/Tests/Pure.RelationalSchema.Tests/ColumnType/TimeColumnTypeTests.cs
@@ -0,0 +1,28 @@
+﻿using Pure.Primitives.Materialized.String;
+using Pure.RelationalSchema.Abstractions.ColumnType;
+using Pure.RelationalSchema.ColumnType;
+
+namespace Pure.RelationalSchema.Tests.ColumnType;
+
+public sealed record TimeColumnTypeTests
+{
+    [Fact]
+    public void ContainCorrectTypeName()
+    {
+        const string expectedTypeName = "time";
+        IColumnType columnType = new TimeColumnType();
+        Assert.Equal(expectedTypeName, new MaterializedString(columnType.Name).Value);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new TimeColumnType().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new TimeColumnType().ToString());
+    }
+}
