552b6a0c261ff429e5b22307aecd404d7803e71f
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-24 15:27:36 +0300

    Implemented TableEqualityComparer.

---
+++ src/Tests/Pure.RelationalSchema.Tests/EqualityComparers/TableEqualityComparer.cs
@@ -0,0 +1,16 @@
+﻿using Pure.Primitives.Materialized.String;
+using Pure.RelationalSchema.Abstractions.Table;
+
+namespace Pure.RelationalSchema.Tests.EqualityComparers;
+
+public sealed record TableEqualityComparer : IEqualityComparer<ITable>
+{
+    public bool Equals(ITable? column, ITable? column1)
+    {
+        return new MaterializedString(column!.Name).Value == new MaterializedString(column1!.Name).Value &&
+               column.Columns.SequenceEqual(column1.Columns, new ColumnEqualityComparer()) &&
+               column.Indexes.SequenceEqual(column1.Indexes, new IndexEqualityComparer());
+    }
+
+    public int GetHashCode(ITable obj) { throw new NotSupportedException(); }
+}
