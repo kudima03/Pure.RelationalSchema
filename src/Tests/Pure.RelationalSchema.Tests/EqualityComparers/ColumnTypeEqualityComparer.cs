2fe8fd5c9d945a24247a63ed3ac011d1d9e077d8
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-24 14:27:33 +0300

    Implemented ColumnTypeEqualityComparer.

---
+++ src/Tests/Pure.RelationalSchema.Tests/EqualityComparers/ColumnTypeEqualityComparer.cs
@@ -0,0 +1,14 @@
+﻿using Pure.Primitives.Materialized.String;
+using Pure.RelationalSchema.Abstractions.ColumnType;
+
+namespace Pure.RelationalSchema.Tests.EqualityComparers;
+
+public sealed record ColumnTypeEqualityComparer : IEqualityComparer<IColumnType>
+{
+    public bool Equals(IColumnType? column, IColumnType? column1)
+    {
+        return new MaterializedString(column!.Name).Value == new MaterializedString(column1!.Name).Value;
+    }
+
+    public int GetHashCode(IColumnType obj) { throw new NotSupportedException(); }
+}
