7b64911450998f9b0878200e8fdb6c86c857cde8
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-24 09:56:38 +0300

    Implemented UShortColumnType.

---
+++ src/Pure.RelationalSchema/ColumnType/UShortColumnType.cs
@@ -0,0 +1,20 @@
+﻿using Pure.Primitives.Abstractions.String;
+using Pure.RelationalSchema.Abstractions.ColumnType;
+using String = Pure.Primitives.String.String;
+
+namespace Pure.RelationalSchema.ColumnType;
+
+public sealed record UShortColumnType : IColumnType
+{
+    IString IColumnType.Name { get; } = new String("ulong");
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
