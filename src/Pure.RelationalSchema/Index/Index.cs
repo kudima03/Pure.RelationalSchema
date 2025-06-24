a3179ccb1e86f5e91afaf135707d7b0a01497590
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-24 12:25:10 +0300

    Implemented Index.

---
+++ src/Pure.RelationalSchema/Index/Index.cs
@@ -0,0 +1,28 @@
+﻿using Pure.Primitives.Abstractions.Bool;
+using Pure.RelationalSchema.Abstractions.Column;
+using Pure.RelationalSchema.Abstractions.Index;
+
+namespace Pure.RelationalSchema.Index;
+
+public sealed record Index : IIndex
+{
+    public Index(IBool isUnique, IEnumerable<IColumn> columns)
+    {
+        IsUnique = isUnique;
+        Columns = columns;
+    }
+
+    public IBool IsUnique { get; }
+
+    public IEnumerable<IColumn> Columns { get; }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
