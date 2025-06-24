86d91f2cdf75abe3ed1e8957ca535c30a97e95e0
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-24 12:01:48 +0300

    Added tests for LongColumnType.

---
+++ src/Tests/Pure.RelationalSchema.Tests/ColumnType/LongColumnTypeTests.cs
@@ -0,0 +1,28 @@
+﻿using Pure.Primitives.Materialized.String;
+using Pure.RelationalSchema.Abstractions.ColumnType;
+using Pure.RelationalSchema.ColumnType;
+
+namespace Pure.RelationalSchema.Tests.ColumnType;
+
+public sealed record LongColumnTypeTests
+{
+    [Fact]
+    public void ContainCorrectTypeName()
+    {
+        const string expectedTypeName = "long";
+        IColumnType columnType = new LongColumnType();
+        Assert.Equal(expectedTypeName, new MaterializedString(columnType.Name).Value);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new LongColumnType().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new LongColumnType().ToString());
+    }
+}
