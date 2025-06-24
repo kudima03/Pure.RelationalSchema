3efbd3916be65d636b7377bf4b94833fabd30feb
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-24 12:01:33 +0300

    Added tests for DateTimeColumnType.

---
+++ src/Tests/Pure.RelationalSchema.Tests/ColumnType/DateTimeColumnTypeTests.cs
@@ -0,0 +1,28 @@
+﻿using Pure.Primitives.Materialized.String;
+using Pure.RelationalSchema.Abstractions.ColumnType;
+using Pure.RelationalSchema.ColumnType;
+
+namespace Pure.RelationalSchema.Tests.ColumnType;
+
+public sealed record DateTimeColumnTypeTests
+{
+    [Fact]
+    public void ContainCorrectTypeName()
+    {
+        const string expectedTypeName = "datetime";
+        IColumnType columnType = new DateTimeColumnType();
+        Assert.Equal(expectedTypeName, new MaterializedString(columnType.Name).Value);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new DateTimeColumnType().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new DateTimeColumnType().ToString());
+    }
+}
