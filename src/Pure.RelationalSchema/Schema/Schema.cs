c0f1ebaf2a8946974c784065817341418608d0d2
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-24 15:40:48 +0300

    Implemented Schema.

---
+++ src/Pure.RelationalSchema/Schema/Schema.cs
@@ -0,0 +1,35 @@
+﻿using Pure.Primitives.Abstractions.String;
+using Pure.RelationalSchema.Abstractions.ForeignKey;
+using Pure.RelationalSchema.Abstractions.Schema;
+using Pure.RelationalSchema.Abstractions.Table;
+
+namespace Pure.RelationalSchema.Schema;
+
+public sealed record Schema : ISchema
+{
+    public Schema(
+        IString name, 
+        IEnumerable<ITable> tables, 
+        IEnumerable<IForeignKey> foreignKeys)
+    {
+        Name = name;
+        Tables = tables;
+        ForeignKeys = foreignKeys;
+    }
+
+    public IString Name { get; }
+
+    public IEnumerable<ITable> Tables { get; }
+
+    public IEnumerable<IForeignKey> ForeignKeys { get; }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
