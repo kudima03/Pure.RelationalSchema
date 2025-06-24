84094bea21f2ff9d72c210639a8c624e6d4903c8
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-24 12:16:55 +0300

    Added tests for Column.

---
+++ src/Tests/Pure.RelationalSchema.Tests/Column/ColumnTests.cs
@@ -0,0 +1,43 @@
+﻿using Pure.Primitives.Abstractions.String;
+using Pure.Primitives.Materialized.String;
+using Pure.Primitives.Number;
+using Pure.Primitives.Random.String;
+using Pure.Primitives.String;
+using Pure.RelationalSchema.Abstractions.Column;
+using Pure.RelationalSchema.Abstractions.ColumnType;
+using Pure.RelationalSchema.ColumnType;
+
+namespace Pure.RelationalSchema.Tests.Column;
+
+using Column = RelationalSchema.Column.Column;
+
+public sealed record ColumnTests
+{
+    [Fact]
+    public void InitializeType()
+    {
+        IColumnType expectedType = new TimeColumnType();
+        IColumn column = new Column(new EmptyString(), expectedType);
+        Assert.Equal(new MaterializedString(expectedType.Name).Value, new MaterializedString(column.Type.Name).Value);
+    }
+
+    [Fact]
+    public void InitializeName()
+    {
+        IString expectedTypeName = new RandomString(new UShort(10));
+        IColumn column = new Column(expectedTypeName, new TimeColumnType());
+        Assert.Equal(new MaterializedString(expectedTypeName).Value, new MaterializedString(column.Name).Value);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new Column(new EmptyString(), new DateColumnType()).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new Column(new EmptyString(), new TimeColumnType()).ToString());
+    }
+}
