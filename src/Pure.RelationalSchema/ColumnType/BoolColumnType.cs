d1e2766b9b3dead7418ae8ac62057adfbf2fcec5
Dmitry Kurochkin <kudima03@gmail.com>
2025-09-22 14:07:42 +0300

    Implemented BoolColumnType

---
+++ src/Pure.RelationalSchema/ColumnType/BoolColumnType.cs
@@ -0,0 +1,20 @@
+using Pure.Primitives.Abstractions.String;
+using Pure.RelationalSchema.Abstractions.ColumnType;
+using String = Pure.Primitives.String.String;
+
+namespace Pure.RelationalSchema.ColumnType;
+
+public sealed record BoolColumnType : IColumnType
+{
+    public IString Name { get; } = new String("bool");
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
