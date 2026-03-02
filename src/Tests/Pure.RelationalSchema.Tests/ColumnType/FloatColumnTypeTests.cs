1f8e10ae9fa8ec871fb90b82a7b97075f77c9dfd
Dmitry Kurochkin <kudima03@gmail.com>
2026-03-02 16:03:55 +0300

    FloatColumnTypeTests implemented

---
+++ src/Tests/Pure.RelationalSchema.Tests/ColumnType/FloatColumnTypeTests.cs
@@ -0,0 +1,29 @@
+using Pure.RelationalSchema.Abstractions.ColumnType;
+using Pure.RelationalSchema.ColumnType;
+
+namespace Pure.RelationalSchema.Tests.ColumnType;
+
+public sealed record FloatColumnTypeTests
+{
+    [Fact]
+    public void ContainCorrectTypeName()
+    {
+        const string expectedTypeName = "float";
+        IColumnType columnType = new FloatColumnType();
+        Assert.Equal(expectedTypeName, columnType.Name.TextValue);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        _ = Assert.Throws<NotSupportedException>(() =>
+            new FloatColumnType().GetHashCode()
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        _ = Assert.Throws<NotSupportedException>(() => new FloatColumnType().ToString());
+    }
+}
